5e04676130a2bb8f4d55cb8aad90f6e31a807dfe
Hayanna Castro <152522247+hayhay07@users.noreply.github.com>
2025-01-30 21:30:36 -0300

    Add files via upload

---
+++ q3.py
@@ -0,0 +1,7 @@
+#Dada a lista frase = ["O", "Python", "é", "uma", "linguagem", "poderosa"], crie uma 
+# nova lista contendo as palavras "Python é uma" usando o fatiamento
+
+lista = ["0", "Python", "é", "uma", "linguagem", "poderosa"]
+
+palavras = lista[1:4:]
+print(palavras)
---
+++ q4.py
@@ -0,0 +1,6 @@
+#4. Crie uma lista com os números de 1 a 20 e use
+# o fatiamento para criar uma lista com todos os números ímpares.
+
+numeros = list(range(1,21))
+num_impares = numeros[ : :2]
+print(num_impares)
